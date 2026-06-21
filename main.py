15a7cf39d919b85a101b25ec8752b301caf83731
Luiz Manoel <luizmanoelramalhosantos74@gmail.com>
2026-06-21 15:50:43 -0300

    Adiciona tratamento de excessões para IPs inválidos e quedas de rede

--- main.py
+++ main.py
@@ -7,21 +7,31 @@ def solicitar_ip():
 
 
 def verificar_porta(ip, porta):
-    conexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conexao.settimeout(1)
-    resultado = conexao.connect_ex((ip, porta))
+    try:
+        conexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conexao.settimeout(1)
+        resultado = conexao.connect_ex((ip, porta))
 
-    if resultado == 0:
-        print(f"Porta {porta}: ABERTA")
-        with open("resultado.txt", "a") as arquivo:
-            arquivo.write(f"Porta {porta}: ABERTA\n")
-    else:
-        print(f"Porta {porta}: FECHADA")
+        if resultado == 0:
+            print(f"Porta {porta}: ABERTA")
+            with open("resultado.txt", "a") as arquivo:
+                arquivo.write(f"Porta {porta}: ABERTA\n")
+        else:
+            print(f"Porta {porta}: FECHADA")
 
-    conexao.close()
+        conexao.close()
+
+    except socket.gaierror:
+        print("Erro: Endereço IP inválido ou não encontrado.")
+        return False
+    except socket.error:
+        print("Erro: Houve um problema de conexão com a rede.")
+        return False
+    return True
 
 
 ip = solicitar_ip()
 
-for porta in range(1, 15):
-    verificar_porta(ip, porta)
+for porta in range(1, 16):
+    if not verificar_porta(ip, porta):
+        break
