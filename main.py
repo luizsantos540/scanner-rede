739d3b3d7c5fd8902f787e87200ed8e0426534ab
Luiz Manoel <luizmanoelramalhosantos74@gmail.com>
2026-06-21 15:27:24 -0300

    Implementa a varredura de múltiplas portas

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+import socket
+
+
 def solicitar_ip():
     ip = input("Digite o endereço IP que deseja verificar: ")
     return ip
 
 
+def verificar_porta(ip, porta):
+
+    conexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    conexao.settimeout(1)
+
+    resultado = conexao.connect_ex((ip, porta))
+
+    if resultado == 0:
+        print(f"Porta {porta}: ABERTA")
+    else:
+        print(f"Porta {porta}: FECHADA")
+
+    conexao.close()
+
+
 ip = solicitar_ip()
 
-print(f"IP informado: {ip}")
+portas = [21, 22, 80, 443]
+
+for porta in portas:
+    verificar_porta(ip, porta)
